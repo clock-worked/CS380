ab69c7ca31bc02b321d53c80a130ca8665ab9a5e
CJ <chad.dotsonjones@gmail.com>
2018-05-02 21:28:05 -0400

    backtrack broken

--- HW1/backTrack.py
+++ HW1/backTrack.py
@@ -1,38 +1,50 @@
 from pegGame import Game
-import random
+def backTrack(stateList):
 
-def backTrack(selectedRules=[]):
-    game.describeState()
+    state = stateList[0]
+
+    if state in stateList[1:]:
+        return
+
+    #dead end
+    if state.applicableRules() == []:
+        return
+
+    if state.goal():
+        return None
+
+    if len(stateList) > 20:
+        return
+
+    state.describeState()
     rules = game.applicableRules()
 
-    for i, rule in enumerate(rules):
-        print"{}:\t{}".format(i, rule)
-    rule = random.choice(rules)
-    game.describeRule(rule)
-
-
-    if rule not in selectedRules:
-        selectedRules.append(rule)
-
-
-    if len(rules) < 2:
-        if game.goal() and game.softApplyRule(rule):
-            return rules
-        else:
-            print "BackTracking..."
-            selectedRules.pop()
-    else:
-        game.applyRule(rule)
-
-    return backTrack(allChoices)
-
-
-
-
+    for rule in rules:
+        newState = state.applyRule(rule)
+        newStateList = stateList.insert(0, newState)
+        path = backTrack(newStateList)
+    #
+    #
+    # if rule not in selectedRules:
+    #     selectedRules.append(rule)
+    #
+    #
+    # if len(rules) < 2:
+    #     if game.goal() and game.softApplyRule(rule):
+    #         return rules
+    #     else:
+    #         print "BackTracking..."
+    #         selectedRules.pop()
+    # else:
+    #     game.applyRule(rule)
+    #
+    # return backTrack(allChoices)
 
 
 game = Game()
 game.refBoard()
-allChoices = []
-backTrack(allChoices)
+backTrack([game])
+
+
+
 
